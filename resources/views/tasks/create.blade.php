206c427be0e7228dbae5b140799ac2d23c85da50
EC2 Default User <ec2-user@ip-172-31-24-62.us-east-2.compute.internal>
2018-05-12 12:42:45 +0000

    kadai

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,20 +3,22 @@
 @section('content')
     
         <h1>新規タスク作製ページ</h1>
-        <div class="row">
-           <div class="col-xs-12　col-sm-offset-8 col-md-offset-8 col-lg-offset-6">
-           {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
-                   <div class="form-group">
-                    {!! Form::label('status', 'ステータス:') !!}
-                    {!! Form::text('status',null,['class'=>'form-control','placeholder'=>'例：休み']) !!}
-                    </div>
-                    
-                    <div class="form-group">
-                    {!! Form::label('content', 'タスク:') !!}
-                    {!! Form::text('content',null,['class'=>'form-control','placeholder'=>'例：整理']) !!}
-                    </div>
-                    {!! Form::submit('作製',['class'=>'btn btn-primary']) !!}
-                {!! Form::close() !!}
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12　col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+               {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
+                       <div class="form-group">
+                        {!! Form::label('status', 'ステータス:') !!}
+                        {!! Form::text('status',null,['class'=>'form-control','placeholder'=>'例：休み']) !!}
+                        </div>
+                        
+                        <div class="form-group">
+                        {!! Form::label('content', 'タスク:') !!}
+                        {!! Form::text('content',null,['class'=>'form-control','placeholder'=>'例：整理']) !!}
+                        </div>
+                        {!! Form::submit('作製',['class'=>'btn btn-primary']) !!}
+                    {!! Form::close() !!}
+               </div>
            </div>
        </div>
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,22 +3,24 @@
 @section('content')
 
     <h1> id:{{$tasks->id}}のタスク編集ページ</h1>
-    <div class="row">
-        <div class="col-xs-12　col-sm-offset-8 col-md-offset-8 col-lg-offset-6">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12　col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             
-    {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
-        <div class="form-group">
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status',null,['class'=>'form-control','placeholder'=>'値が入力されていません 例：休み']) !!}
+        {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
+            <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status',null,['class'=>'form-control','placeholder'=>'値を入力してください 例：休み']) !!}
+            </div>
+            <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content',null,['class'=>'form-control','placeholder'=>'値を入力してください 例：整理']) !!}
+            </div>
+            
+            {!! Form::submit('更新',['class'=>'btn btn-default']) !!}
+            
+             </div>
         </div>
-        <div class="form-group">
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content',null,['class'=>'form-control','placeholder'=>'値が入力されていません 例：整理']) !!}
-        </div>
-        
-        {!! Form::submit('更新',['class'=>'btn btn-default']) !!}
-        
-         </div>
     </div>
 
     {!! Form::close() !!}
